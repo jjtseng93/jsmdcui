af8e03c871a2322084e5b9a082dab2e65c3937af
Dr. Xiaozhi <jjtseng93@gmail.com>
2026-07-20 03:39:55 +0800

    clean message

--- clean.sh
+++ clean.sh
@@ -2,5 +2,7 @@
 
 sd=$(dirname "$0")
 
-rm "$sd"/*.md.* "$sd"/*.md-*
-rm "$sd"/demos/*.md.* "$sd"/demos/*.md-*
+rm "$sd"/*.md.* "$sd"/*.md-* 2>/dev/null
+rm "$sd"/demos/*.md.* "$sd"/demos/*.md-* 2>/dev/null
+
+echo "Removed all *.md.* *.md-* from repo root and demos/ folder"
