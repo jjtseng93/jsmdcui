87b1d6561b07ea22b406bbd0cdd26ba7e79323ce
Dr. Xiaozhi <jjtseng93@gmail.com>
2026-07-20 03:47:13 +0800

    clean tests folder

--- clean.sh
+++ clean.sh
@@ -4,5 +4,6 @@ sd=$(dirname "$0")
 
 rm "$sd"/*.md.* "$sd"/*.md-* 2>/dev/null
 rm "$sd"/demos/*.md.* "$sd"/demos/*.md-* 2>/dev/null
+rm "$sd"/tests/*.md.* "$sd"/tests/*.md-* 2>/dev/null
 
 echo "Removed all *.md.* *.md-* from repo root and demos/ folder"
